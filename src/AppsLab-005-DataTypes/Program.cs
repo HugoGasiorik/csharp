fa5f992dff82a64384204054448a7f65e6973603
gasiorikh25 <gasiorikh25@Z00T1-15>
2025-09-10 10:05:08 +0200

    5 zakladnych prikazov

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 15;
+            Console.WriteLine(myAge);
+
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+
+            string name = "niggaboy";
+
+            Console.WriteLine(name);
+
+            char gender = 'M';
+            Console.WriteLine(gender);
+
+            bool isAdult = false; 
+                Console.WriteLine(isAdult);
+        }
+    }
+}
+
+
+
