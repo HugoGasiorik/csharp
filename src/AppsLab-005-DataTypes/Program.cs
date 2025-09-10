10a6195e77353037487f7d43fa0e68e2655b918a
gasiorikh25 <gasiorikh25@Z00T1-15>
2025-09-10 10:09:23 +0200

    zaciatky ucenire

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -6,20 +6,23 @@ namespace MyApp
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   // cislo 
             int myAge = 15;
             Console.WriteLine(myAge);
-
+            
+            //desatinne cislo
             float pi = 3.14f;
             Console.WriteLine(pi);
-
+            
+            //hoci co v pomlckach
             string name = "niggaboy";
-
             Console.WriteLine(name);
-
+          
+            //jeden znak
             char gender = 'M';
             Console.WriteLine(gender);
-
+            
+            //true flase
             bool isAdult = false; 
                 Console.WriteLine(isAdult);
         }
