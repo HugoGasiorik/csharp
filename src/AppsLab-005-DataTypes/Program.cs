22e6d62f2973212579670af38d69d2c997798b25
gasiorikh25 <gasiorikh25@Z00T1-15>
2025-09-10 13:09:07 +0200

    10.9. vsetky skratky sucty

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -6,7 +6,9 @@ namespace MyApp
     internal class Program
     {
         static void Main(string[] args)
-        {   // cislo 
+        {   
+            /*
+            // cislo 
             int myAge = 15;
             Console.WriteLine(myAge);
             
@@ -24,10 +26,72 @@ namespace MyApp
             
             //true flase
             bool isAdult = false; 
-                Console.WriteLine(isAdult);
+                Console.WriteLine(isAdult); 
+
+
+
+
+           //scitavanie A B 
+            int a = 6;
+            int b = 5;
+            int sum = a + b;
+            Console.WriteLine(sum);
+
+
+           
+            Console.WriteLine(a - b);
+            //odcitanie 
+            int sum2 = a - b;
+            Console.WriteLine(sum2);
+
+            //nasobenie delenie 
+            Console.WriteLine(a * b );
+            Console.WriteLine(a /  b );
+
+            Console.WriteLine("Hugo");*/
+
+
+
+
+            int birthday = 17;
+            int birtMonth = 2;
+            int birthYear = 2010;
+
+            int Birthsum = birthday + birtMonth + birthYear; 
+
+            Console.WriteLine(Birthsum);
+
+
+            Console.WriteLine(Birthsum * 10);
+            Console.WriteLine((birthday+birtMonth) * birthYear);
+            Console.WriteLine(5/3f);
+
+            string Name = "Hugo";
+            int Byear = 2010;
+
+            Console.WriteLine(Name + birthYear);
+
+            Console.WriteLine(birthday+birtMonth+Name);
+
+            bool result = 6 > 3;
+            Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
-
 
 
