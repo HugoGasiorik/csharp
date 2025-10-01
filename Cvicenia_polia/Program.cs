94783254fcd1d67c4a441fb8c4afb66b0cee061d
gasiorikh25 <gasiorikh25@Z00T1-13>
2025-10-01 13:00:29 +0200

    vypisovanie do konzoly

--- Cvicenia_polia/Program.cs
+++ Cvicenia_polia/Program.cs
@@ -1,10 +1,79 @@
-﻿namespace Cvicenia_polia
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cvicenia_polia
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {/*
+            int[] numbers = new int[7];
+
+            numbers[0] = 10;
+            numbers[1] = 15;
+            numbers[2] = 35;
+            numbers[3] = 48;
+            numbers[4] = 2;
+            numbers[5] = 1;
+            numbers[6] = 19;
+            /*
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+                Console.WriteLine(numbers[i]);
+                sum += numbers[i];
+            }
+            
+            
+                Console.WriteLine(sum);
+            */
+
+            /*
+            int sum = 0;
+            foreach (var number in numbers) 
+
+            {
+                sum += number;
+            }
+            Console.WriteLine(sum);
+            */
+
+            /*
+            int[] numbers = new int[5];
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+                numbers[i] = 8;
+            }
+
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+                Console.WriteLine(sum);
+            */
+            //Vypisovanie do konzoly 
+            /*
+            Console.WriteLine("KOlko cisel chces?");
+            int count = int.Parse(Console.ReadLine());
+
+
+            int[] numbers = new int[count];
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Zadaj cislo");
+                string input = Console.ReadLine();
+                numbers[i] = int.Parse(input);
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("Súčet vsetkych cisel je: " + sum);
+            
+            */
+
+            
+
         }
     }
 }
